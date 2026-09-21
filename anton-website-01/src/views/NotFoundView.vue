c94601f4ccd6432f65fb04f5e5cf9bcc211a44e4
JackR1p <43586753+JackR1p@users.noreply.github.com>
2026-09-21 23:55:44 +0200

    Created NotFoundView.vue
    Created NotFoundView.vue

---
+++ anton-website-01/src/views/NotFoundView.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { useI18n } from 'vue-i18n'
+
+import BaseButton from '../components/ui/BaseButton.vue'
+
+const { t } = useI18n()
+</script>
+
+<template>
+  <section class="section page not-found">
+    <div class="container not-found__content">
+      <p class="not-found__code">404</p>
+
+      <h1 class="not-found__title">
+        {{ t('notFound.title') }}
+      </h1>
+
+      <p class="not-found__description">
+        {{ t('notFound.description') }}
+      </p>
+
+      <BaseButton to="/">
+        {{ t('notFound.backHome') }}
+      </BaseButton>
+    </div>
+  </section>
+</template>
